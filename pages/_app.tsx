17d7fbb242aa74f1e256913ad9843caccd103ce0
Jerry Bony <info@techluminateacademy.com>
2025-06-30 21:17:35 -0300

    removed bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import { SessionProvider } from "next-auth/react";
-// 1) Bootstrap's CSS (for grid, nav, tabs, etc.)
-import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
